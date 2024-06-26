7252b9d7f805fd2ff5b250c8651acf90c8c51f2d
Alihan Tasci <alihan.tasci@esarj.com>
2024-06-26 11:52:09 +0300

    watchit finished

--- watchit/index.js
+++ watchit/index.js
@@ -1,3 +1,36 @@
 #!/"usr/bin/env node
 
-console.log("i was executed");
+import chokidar from "chokidar";
+import debounce from "lodash.debounce";
+import program from "caporal";
+import fs from "fs";
+import { spawn } from "child_process";
+
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Couldn't find a file as "${name}"`);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill;
+      }
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
