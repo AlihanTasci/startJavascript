0f80d8e322643fc34de389c29d0aed17a6172c24
Alihan Tasci <alihan.tasci@esarj.com>
2024-07-01 17:12:28 +0300

    users.json finished

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import usersRepo from "./repositories/users";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +18,12 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
   res.send("Account Created");
 });
 app.listen(3000, () => {
