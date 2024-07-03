657fa7aeb9d38c6381d3d0930807729f9b8d9de4
Alihan Tasci <alihan.tasci@esarj.com>
2024-07-03 09:59:13 +0300

    cookie session added

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-import "./repositories/users.js";
+import { default as usersRepo } from "./repositories/users.js";
+import cookieSession from "cookie-session";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["aldfkmvaldfkv"]
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(`
   <div>
+    Your id is ${req.session.userId}
     <form method="POST">
       <input name="email" placeholder="email" />
       <input name="password" placeholder="password" />
@@ -24,6 +31,12 @@ app.post("/", async (req, res) => {
   if (existingUser) {
     return res.send("Email in use");
   }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Password must match");
+  }
+  const user = await usersRepo.create({ email, password });
+  req.session.userId = user.id;
   res.send("Account Created");
 });
 app.listen(3000, () => {
